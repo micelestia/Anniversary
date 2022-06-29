eb0368ae60a0e11ebeac040650d3a6cf61dae952
miyufi <94786606+micelestia@users.noreply.github.com>
2022-06-30 02:09:58 +0800

    Initial commit

---
+++ js/commands.js
@@ -0,0 +1,252 @@
+var youtube = "https://www.youtube.com/watch?v=Egb3saw6vkU";
+var twitter = "https://twitter.com/Mememerfe";
+var password = "lune";
+var instagram = "https://www.instagram.com/mememerfe/";
+var facebook = "https://www.facebook.com/kianne.erfe";
+var valentines = "https://sunflowers-for-lune.netlify.app"
+var text = "https://micelestia.github.io/lune/lune.html"
+
+whois = [
+  "<br>",
+  "Hallu ako si Kianne, jowa mo mwamwa.",
+  "<br>",
+  "Nakilala mo noong May 1, 2021 tapos naging jowa mo noong July 1, 2021. Super ",
+  "simp sayo, super faithful sayo, super love na love ikaw, at syempre super swerte ",
+  "dahil napunta na rin ako sayo. July 22, 2000 birthday ko tapos Computer Science ",
+  "course ko ngayon. Gradwaiting na ngayon at soon to be sugar daddy mo hehe. ",
+  "Future na asawa mo at makakasama mo hanggang pagtanda. Magkakaanak din ",
+  "tayo kung ilan gusto mo tapos papatayo bahay na maganda super tapos ",
+  "magtratravel habang wala pang anak. I love you so much. ",
+  "<br>",
+];
+
+whoami = [
+  "<br>",
+  "Hallu ako si Nica, jowa ni Kianne mwamwa.",
+  "<br>",
+  "May 14, 2001 ang birthday ko tapos Taurus ang zodiac sign ko. Magaling na ",
+  "dancer, maganda super, sexy talaga, at matalino. Mahal na mahal ni Kianne at ",
+  "talagang mahal na mahal ko rin siya. Educ ang course tapos Major in Filipino kaya ",
+  "kapag nagsabi si Kianne ng nanaman nagagalit ako at cinocorrect ko. Mahilig sa ",
+  "Milktea at Takoyaki at super dalas magcrave sa ganung food. Crush si Taehyung ",
+  "kasi syempre si Kianne ang mahal niya dahil si Kianne ang pinakapogi sa paningin ",
+  "niya. Dapat lang. ",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' +
+    twitter +
+    '" target="_blank">twitter/Mememerfe' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="_blank">instagram/mememerfe' +
+    "</a>",
+  'facebook       <a href="' +
+    facebook +
+    '" target="_blank">facebook/kianne.erfe' +
+    "</a>",
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">anniv</span>           Ano kaya ito?',
+  "<br>",
+];
+
+anniv = [
+  "<br>",
+  "HAPPY 1ST ANNIVERSARY MAHAL!",
+  "<br>",
+  "I love you so much mahal ko. Swerte talaga ako at napunta ako sayo mahal dahil ",
+  "super inaalagaan mo ako. Hindi mo ako pinapabayaan at talagang pinaparamdam ",
+  "mo na sobrang mahal mo ako. Tinatama mo kung anong mga mali ko at sinasabi ",
+  "mo rin ano mga nagiging mali ko na dapat kong baguhin.  Sobrang saya ko talaga ",
+  "araw araw dahil sayo kasi kahit yung simpleng presensya mo lang na nasa call ",
+  "ikaw or mga sinesend mong selfies, natutuwa na ako dahil gustong gusto kita na ",
+  "nakikita hehe. Malapit na tayong magkita mahal, malapit na talaga sobra. Tiis tiis ",
+  "na lang mahal sa ngayon dahil hindi pa makaalis, pero kapag pwede na makaalis ",
+  "pupuntahan na talaga kita diyan at gagawin natin lahat ng mga plano natin na ",
+  "gawin. Magiipon muna ang mahal para magawa natin yung mga plano natin at ",
+  "para magtagal ako nang sobra diyan sa inyo. Nandito lang ang mahal palagi para ",
+  "sayo, hinding hindi kita papabayaan at lahat ng problema mo ay problema ko rin. ",
+  "Dadamayan kita sa mga problema mo at balang araw ay masosolusyonan na rin ",
+  "ang mga problema mo na yun at talagang magkakaroon ka na ng peace of mind ",
+  "mahal. Yun talaga pinakawish ko sayo, ang magkaroon ikaw ng peace of mind mo, ",
+  "para hindi na rin sumakit ang ulo mo palagi at mapagod araw araw tuwing ",
+  "nandiyan ikaw. Habang hindi pa nangyayari yun, nandito lang ako para sayo ",
+  "mahal. Kahit din naman na mangyari na yun nandito pa rin ako para sayo, hinding ",
+  "hindi ako mawawala sayo. Mahal na mahal kita sobra. Happy 1st Anniversary ulit ",
+  "mahal. More anniversaries to come hehe. I love you so much. ♥",
+  "<br>",
+];
+
+gifts = [
+  "<br>",
+  "twosday        Gift ko sayo noong twosday hehe",
+  'valentines     <a href="' +
+    valentines +
+    '" target="_blank">sunflowers-for-lune' +
+    "</a>",
+  'text           <a href="' +
+    text +
+    '" target="_blank">github/lune' +
+    "</a>",
+  "<br>",
+];
+
+twosday = [
+  "<br>",
+  "                                               _   _",
+  "       _______________                        |*&#92;_/*|________",
+  "      |  ___________  |     .-.     .-.      ||_/-&#92;_|______  |",
+  "      | |           | |    .****. .****.     | |           | |",
+  "      | |   0   0   | |    .*****.*****.     | |   0   0   | |",
+  "      | |     -     | |     .*********.      | |     -     | |",
+  "      | |   &#92;___/   | |      .*******.       | |   &#92;___/   | |",
+  "      | |___     ___| |       .*****.        | |___________| |",
+  "      |_____|&#92;_/|_____|        .***.         |_______________|",
+  "        _|__|/ &#92;|_|_.............*.............._|________|_",
+  "       / ********** &#92;                          / ********** &#92;",
+  "     /  ************  &#92;                      /  ************  &#92;",
+  "    --------------------                    --------------------",
+  "Hallu mahal ko hehe I love you so much, this is us dahil",
+  "sa malayo tayo sa isa't isa. Happy two's day mahal koooo",
+  "I know this isn't much pero sana magustuhan mo pa rin",
+  "Sana this still makes our day special ngayong 2/22/22",
+  "Tapos saktong 2:22pm din hehe mahal na mahal kita",
+  "Di ako magsasawang sabihin sayo na sobrang swerte ko",
+  "Dahil swerte naman talaga ako sayo at masaya ako dun",
+  "Kuntento na ako sayo wala na rin akong hahanapin sa iba ",
+  "Sana mahal ay hanggang dulo hanggang tumanda tayo",
+  "Tayo pa rin hehe sobrang mahal na mahal kita enjoy mahal",
+  "Happy two's day ulit mahal, let's make this day memorable",
+  "<br>",
+  "              ******       ******",
+  "            **********   **********",
+  "          ************* *************",
+  "         *****************************",
+  "         *****************************",
+  "         *****************************",
+  "          ***************************",
+  "            ***********************",
+  "              *******************",
+  "                ***************",
+  "                  ***********",
+  "                    *******",
+  "                      ***",
+  "                       *",
+  "<br>",
+  "     ******       ******",
+  "   **********   **********",
+  " ************* *************",
+  "*****************************",
+  "*****************************",
+  "*****************************",
+  " ***************************",
+  "   ***********************",
+  "     *******************",
+  "       ***************",
+  "         ***********",
+  "           *******",
+  "             ***",
+  "              *",
+  "<br>",
+  "        @@@@@@           @@@@@@",
+  "      @@@@@@@@@@       @@@@@@@@@@",
+  "    @@@@@@@@@@@@@@   @@@@@@@@@@@@@@",
+  "  @@@@@@@@@@@@@@@@@ @@@@@@@@@@@@@@@@@",
+  " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "      @@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "        @@@@@@@@@@@@@@@@@@@@@@@",
+  "          @@@@@@@@@@@@@@@@@@@",
+  "            @@@@@@@@@@@@@@@",
+  "              @@@@@@@@@@@",
+  "                @@@@@@@",
+  "                  @@@",
+  "                   @",
+  "<br>",
+  "                       @@@@@@           @@@@@@",
+  "                     @@@@@@@@@@       @@@@@@@@@@",
+  "                   @@@@@@@@@@@@@@   @@@@@@@@@@@@@@",
+  "                 @@@@@@@@@@@@@@@@@ @@@@@@@@@@@@@@@@@",
+  "                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "               @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "               @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "               @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "                 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "                  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "                     @@@@@@@@@@@@@@@@@@@@@@@@@@@",
+  "                       @@@@@@@@@@@@@@@@@@@@@@@",
+  "                         @@@@@@@@@@@@@@@@@@@",
+  "                           @@@@@@@@@@@@@@@",
+  "                             @@@@@@@@@@@",
+  "                               @@@@@@@",
+  "                                 @@@",
+  "                                  @",
+  "<br>",
+  "                       ________           ________",
+  "                     /  ______  &#92;       /  ______  &#92;",
+  "                   /  /        &#92;  &#92;   /  /        &#92;  &#92;",
+  "                 /  /            &#92;  '  /            &#92;  &#92;",
+  "                |  |               &#92; /               |  |",
+  "                |  |                '                |  |",
+  "                |  |                                 |  |",
+  "                 |  |                               |  |",
+  "                  |  |                             |  |",
+  "                    |  |                         |  |",
+  "                      |  |                     |  |",
+  "                        |  |                 |  |",
+  "                          |  |             |  |",
+  "                            |  |         |  |",
+  "                              |  |     |  |",
+  "                                |  | |  |",
+  "                                  | | |",
+  "                                   | |",
+  "                                    |",
+  "                        I love youuuuu mwamwa <3",
+  "<br>",
+];
+
+bebe = [
+  "<br>",
+  '<span class="command">whois</span>          Sino nga ba si bebe?',
+  '<span class="command">whoami</span>         Sino naman ikaw bebe?',
+  '<span class="command">video</span>          Tahanan hehe',
+  '<span class="command">social</span>         Mga soc meds ko kung nasan nagsisimp ako sayo',
+  '<span class="command">secret</span>         Hanapin mo dito sa website bebe hehe',
+  '<span class="command">gifts</span>          Mga dati kong naibigay sayo',
+  '<span class="command">bebe</span>           Dats mi',
+  '<span class="command">clear</span>          Linis kalat lang sa terminal',
+  '<span class="command">banner</span>         Display ulit yung happy anniv hehe',
+  "<br>",
+];
+
+banner = [
+  "    __  __                               ___       __     ___                   _                                             ",
+  "   / / / /____ _ ____   ____   __  __   <  /_____ / /_   /   |   ____   ____   (_)_   __ ___   _____ _____ ____ _ _____ __  __",
+  "  / /_/ // __ `// __ &#92; / __ &#92; / / / /   / // ___// __/  / /| |  / __ &#92; / __ &#92; / /| | / // _ &#92; / ___// ___// __ `// ___// / / /",
+  "&nbsp;/ __  // /_/ // /_/ // /_/ // /_/ /   / /(__  )/ /_   / ___ | / / / // / / // / | |/ //  __// /   (__  )/ /_/ // /   / /_/ / ",
+  "/_/ /_/ &#92;__,_// .___// .___/ &#92;__, /   /_//____/ &#92;__/  /_/  |_|/_/ /_//_/ /_//_/  |___/ &#92;___//_/   /____/ &#92;__,_//_/    &#92;__, /  ",
+  "            /_/    /_/     /____/                                                                                   /____/   ",
+  "    __  ___        __            __ __   ____   __                                             __   _____",
+  "   /  |/  /____ _ / /_   ____ _ / // /  /  _/  / /____  _   __ ___     __  __ ____   __  __   / /  |__  /",
+  "  / /|_/ // __ `// __ &#92; / __ `// // /   / /   / // __ &#92;| | / // _ &#92;   / / / // __ &#92; / / / /  / /    /_ < ",
+  "&nbsp;/ /  / // /_/ // / / // /_/ // //_/  _/ /   / // /_/ /| |/ //  __/  / /_/ // /_/ // /_/ /   &#92; &#92;  ___/ / ",
+  "/_/  /_/ &#92;__,_//_/ /_/ &#92;__,_//_/(_)  /___/  /_/ &#92;____/ |___/ &#92;___/   &#92;__, / &#92;____/ &#92;__,_/     &#92;_&#92;/____/  ",
+  "                                                                    /____/                               ",
+  '<span class="color2">Welcome mahal ko! </span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'bebe\'</span><span class="color2">.</span>',
+];
---
+++ js/main.js
@@ -0,0 +1,188 @@
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer");
+var textarea = document.getElementById("texter");
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+let pwd = false;
+var commands = [];
+
+setTimeout(function () {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+console.log(
+  "%cHallu mahal ko hehe ",
+  "color: #04ff00; font-weight: bold; font-size: 24px;"
+);
+console.log("%cPassword: '" + password + "' - mwamwa", "color: grey");
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+  if (pw) {
+    let et = "*";
+    let w = textarea.value.length;
+    command.innerHTML = et.repeat(w);
+    if (textarea.value === password) {
+      pwd = true;
+    }
+    if (pwd && e.keyCode == 13) {
+      loopLines(secret, "color2 margin", 120);
+      command.innerHTML = "";
+      textarea.value = "";
+      pwd = false;
+      pw = false;
+      liner.classList.remove("password");
+    } else if (e.keyCode == 13) {
+      addLine("Maling password bebe", "error", 0);
+      command.innerHTML = "";
+      textarea.value = "";
+      pw = false;
+      liner.classList.remove("password");
+    }
+  } else {
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine("anniv@bebe.mwamwa:~$ " + command.innerHTML, "no-animation", 0);
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+}
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "bebe":
+      loopLines(bebe, "color2 margin", 80);
+      break;
+    case "whois":
+      loopLines(whois, "color2 margin", 80);
+      break;
+    case "whoami":
+      loopLines(whoami, "color2 margin", 80);
+      break;
+    case "video":
+      addLine("Opening YouTube...", "color2", 80);
+      newTab(youtube);
+      break;
+    case "anniv":
+      loopLines(anniv, "color2 margin", 80);
+      break;
+    case "social":
+      loopLines(social, "color2 margin", 80);
+      break;
+    case "secret":
+      liner.classList.add("password");
+      pw = true;
+      break;
+    case "gifts":
+      loopLines(gifts, "color2 margin", 80);
+      break;
+    case "twosday":
+      loopLines(twosday, "color2 margin", 80);
+      break;
+    case "password":
+      addLine(
+        '<span class="inherit"> Engkkkk password ka diyan</span>',
+        "error",
+        100
+      );
+      break;
+    case "clear":
+      setTimeout(function () {
+        terminal.innerHTML = '<a id="before"></a>';
+        before = document.getElementById("before");
+      }, 1);
+      break;
+    case "valentines":
+      addLine("Opening valentines...", "color2", 0);
+      newTab(valentines);
+      break;
+    case "text":
+      addLine("Opening text...", "color2", 0);
+      newTab(text);
+      break;
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+    case "twitter":
+      addLine("Opening Twitter...", "color2", 0);
+      newTab(twitter);
+      break;
+    case "instagram":
+      addLine("Opening Instagram...", "color2", 0);
+      newTab(instagram);
+      break;
+    case "facebook":
+      addLine("Opening Facebook...", "color2", 0);
+      newTab(facebook);
+      break;
+    default:
+      addLine(
+        '<span class="inherit">Command not found. For a list of commands, type <span class="command">\'bebe\'</span>.</span>',
+        "error",
+        100
+      );
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function () {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function () {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function (item, index) {
+    addLine(item, style, index * time);
+  });
+}
